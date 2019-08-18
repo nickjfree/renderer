97ef9b932280dfc9510935f6763e72a29075265a
root <nick12@live.cn>
2019-08-18 21:29:42 +0800

    update

--- Engine/shaders/animation/skinning.hlsl
+++ Engine/shaders/animation/skinning.hlsl
@@ -24,7 +24,7 @@ PS_Input_Simple VS_Skinning_Simple(VS_Input_Skinning vs_input)
     // do bone tranformation
     vs_input = transform_bone(vs_input);
     // transform to pixel shader
-    PS_Input_GBuffer output;
+    PS_Input_Simple output;
     output = transform_to_view_simple(vs_input.PosL, vs_input.Normal, 
         vs_input.TexCoord, vs_input.Tangent, gWorldViewProjection, gWorldViewMatrix);
     return output;
